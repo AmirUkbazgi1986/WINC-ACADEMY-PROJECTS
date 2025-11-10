35ddf03e8d2e401a9c8054ef7a021dcf423c8a48
amir ukbazgi <amirukbazgi@gmail.com>
2025-11-10 23:08:48 +0100

    some modification has been made

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-let isDrunk; // Can be true or false.
-let wearsCoolClothes; // Can be true or false.
-let clubIsFull; // Can be true or false.
+"use strict";
+const header = document.querySelector(".log h1");
+const imageLogo = document.querySelector(".log img");
 
-// Zafar
-// isDrunk = false;
-// wearsCoolClothes = true;
-// clubIsFull = true;
+// imageLogo.addEventListener("click", function () {
+//   const answer = prompt("what is your name?");
+//   header.innerHTML = `Hello ${answer}, How are you?`;
+//   let sign = prompt("what is your  sign?");
+//   if (sign === null) {
+//     header.innerHTML = "oky, may be next time!";
+//   } else if (sign.toLowerCase() === "") {
+//     header.innerHTML = "Don't shy, enter your sign!";
+//   } else if (sign.toLowerCase() === "scorpio") {
+//     header.innerHTML = "Wow! I am a Scorpio too";
+//   } else {
+//     header.innerHTML = `${sign} is my favorite sign!`;
+//   }
+// });
 
-//Storm;
-// isDrunk = true;
-// wearsCoolClothes = true;
-// clubIsFull = true;
-
-// Carla
-// isDrunk = false;
-// wearsCoolClothes = false;
-// clubIsFull = true;
-
-// Alice
-isDrunk = false;
-wearsCoolClothes = false;
-clubIsFull = false;
-
-if (isDrunk) {
-  console.log("Please come back when you are sober");
-} else {
-  if (clubIsFull) {
-    if (wearsCoolClothes) {
-      console.log("Wow, cool clothes, come in");
-    } else {
-      console.log("No, too busy right now");
-    }
-  } else {
-    console.log("Welcome");
-  }
-}
+const generateRandomNum = () => Math.floor(Math.random() * 10);
+const x1 = String(generateRandomNum());
+const x2 = String(generateRandomNum());
+const x3 = String(generateRandomNum());
+const x4 = String(generateRandomNum());
+const x5 = String(generateRandomNum());
+const x6 = String(generateRandomNum());
+console.log(`${x1}${x2}${x3}${x4}${x5}${x6}`);
